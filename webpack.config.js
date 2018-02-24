e12f6c8210b03dd2a9e8fd235839d0978b1f4f53
Mark <mark@remarkablemark.org>
2018-02-24 00:04:16 -0500

    chore(webpack): add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+const paths = {
+  build: path.resolve('./build/'),
+  src: path.resolve('./src/'),
+  template: path.resolve('./src/index.html'),
+};
+
+module.exports = (env = { NODE_ENV: 'production' }) => ({
+  entry: {
+    bundle: './src/index.js',
+  },
+
+  output: {
+    path: paths.build,
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [paths.src],
+        use: ['babel-loader'],
+      },
+    ],
+  },
+
+  // https://webpack.js.org/configuration/devtool/
+  devtool: env.NODE_ENV === 'development' ? 'cheap-module-eval-source-map' : false,
+
+  plugins: [
+    // remove `build` folder
+    new CleanWebpackPlugin([paths.build]),
+
+    // pass environment variable value to app
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+    }),
+
+    // do not bundle certain modules in production
+    env.NODE_ENV === 'production' && new webpack.IgnorePlugin(/^(react-hot-loader)$/),
+
+    // hoist all modules to a single scope instead of writing a separate closure for each
+    env.NODE_ENV === 'production' && new webpack.optimize.ModuleConcatenationPlugin(),
+
+    // display module names when using HMR
+    env.NODE_ENV === 'development' && new webpack.NamedModulesPlugin(),
+
+    // development sandbox
+    env.NODE_ENV === 'development' && new HtmlWebpackPlugin({
+      template: paths.template,
+    }),
+  ].filter(Boolean),
+});
